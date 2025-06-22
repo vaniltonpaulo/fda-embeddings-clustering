48f64c0f4fe37d38fa59ed60626553286a3dee08
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-06-22 11:41:50 +0200

    Visualization of COVID-19 and all-cause mortality

--- chapter-1/second-plot.R
+++ chapter-1/second-plot.R
@@ -2,7 +2,6 @@
 #  2 · 2020 excess deaths vs. COVID-19 deaths
 # ────────────────────────────────
 covid     <- COVID19$US_weekly_mort_CV19
-
 excess    <- COVID19$US_weekly_excess_mort_2020
 weeks2020 <- COVID19$US_weekly_excess_mort_2020_dates
 
@@ -20,17 +19,18 @@ plot_df <- tibble(
   curve      = death_tf
 )
 
+
 ggplot(plot_df, aes(y = curve, colour = death_type)) +
-  geom_meatballs(size = 2) +
+  geom_meatballs(size = 3) +
   scale_colour_manual(values = c("blue", "red")) +
   scale_x_continuous(
+    limits = c(0, max(num_grid(weeks2020))),  # force axis to start at 0
     breaks = num_grid(weeks2020)[seq(1, length(weeks2020), 13)],
     labels = format(weeks2020[seq(1, length(weeks2020), 13)], "%b\n%Y"),
-    expand = c(0, 0)
-  ) +
-  labs(
+    expand = c(0.01,0.1) # This line removes the gap(not really)
+  ) + #coord_cartesian(ylim = c(0, NA), expand = FALSE)+
+labs(
     x = "Weeks starting in January 2020",
     y = "All-cause excess and COVID-19 deaths in the US"
   ) +
-  theme_classic() +
-  theme(legend.position.inside = c(.15, .90))
+  theme_classic() 
